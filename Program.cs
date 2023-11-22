8334f9c1108ff8f2b19b26773259c5196ce78222
ttw1St0fF4te <144245945+ttw1St0fF4te@users.noreply.github.com>
2023-11-22 11:09:32 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,56 +1,40 @@
-﻿using Converter_C;
-using Newtonsoft.Json;
-using System.Security.Cryptography;
-using System.Xml.Serialization;
-
 namespace Converter_C
 {
     class Program
     {
-        Human human = new Human();
-
         static void Main()
         {
-            Console.WriteLine("Введите путь к файлу, который хотите прочитать: ");
+            Console.WriteLine("Введите путь к файлу, который хотите прочитать: "); //C:\Users\User\Desktop\lalala.txt
             Console.WriteLine("-----------------------------------------------");
             string path = Console.ReadLine();
-            bool result = path.Contains(".txt");
-            bool result1 = path.Contains(".json");
-            bool result2 = path.Contains(".xml");
+            Console.Clear();
+            Console.WriteLine("Введите операцию: ");
+            Console.WriteLine("1 - Text to Json");
+            Console.WriteLine("2 - Text to Xml");
+            Console.WriteLine("3 - Json to Text");
+            Console.WriteLine("4 - Xml to Text");
+            int answer = int.Parse(Console.ReadLine());
 
-            if (result == true)
-            {
-                List<Human> txt_list = new List<Human>();
-                string[] strings = File.ReadAllLines(path);
-                for (int i = 0; i <= strings.Length; i += 3)
-                {
-                    Human human = new Human(strings[i], Convert.ToInt32(strings[i += 1]), strings[i += 2]);
-                    txt_list.Add(human);
-                }
-                foreach (Human output_txt in txt_list) { Console.WriteLine(output_txt); }
-            }
+            Converter converter = new Converter();
 
-            if (result1 == true)
+            switch (answer)
             {
-                string text = File.ReadAllText(path);
-                List<Human> humans = JsonConvert.DeserializeObject<List<Human>>(text);
-                string _result = "";
-                for (int i = 0; i < humans.Count; i++)
-                    _result += $"{humans[i].m_strName}\n{humans[i].m_iAge}\n{humans[i].m_strColor}\n";
-                Console.WriteLine(_result);
-            }
-
-            if (result2 == true)
-            {
-                XmlSerializer xmls = new XmlSerializer(typeof(List<Human>));
-                TextReader text_reader = new StreamReader(path);
-                List<Human> xml_list;
-                xml_list = (List<Human>)xmls.Deserialize(text_reader);
-                text_reader.Close();
-                foreach (Human i in xml_list)
-                    Console.WriteLine(i);
-            }
-            Console.ReadKey();
+                case 1:
+                    converter.TextToJson(path);
+                    break;
+                case 2:
+                    converter.TextToXml(path);
+                    break;
+                case 3:
+                    converter.JsonToText(path);
+                    break;
+                case 4:
+                    converter.XmlToText(path);
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }     
         }
     }
 }
